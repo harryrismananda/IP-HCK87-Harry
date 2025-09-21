9f8c527670434dc077c876ceb2cd95250b79f415
harryrismananda <harryrismananda@gmail.com>
2025-09-21 19:16:10 +0700

    chore: add new restriction to AI prompt

--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -1,5 +1,5 @@
 const { GoogleGenAI } = require("@google/genai");
-const { Course, Question } = require("../models");
+const { Course, Question, Language } = require("../models");
 const ai = new GoogleGenAI({});
 class CourseController {
   static async getAllCourses(req, res, next) {
@@ -53,20 +53,21 @@ class CourseController {
     try {
       // console.log("TEST");
       const { language } = req.body;
+      const foundLanguage = await Language.findOne({where: {name: language}})
       const response = await ai.models.generateContent({
         model: "gemini-1.5-pro",
         contents: `
-Role: You are a certified ${language} teacher. 
+Role: You are a certified ${foundLanguage.name} teacher. 
 Instructions: 
 1. Create 3 structured courses (1 Beginner, 1 Intermediate, 1 Advanced). 
    Each course must include:
      - "title": Course name,
      - "difficulty": Beginner | Intermediate | Advanced,
-     - "languageId": (1 = English, 2 = Japanese, 3 = Russian),
+     - "languageId": ${foundLanguage.id},
      - "content": {
           "roadmap": High-level learning plan,
           "lessons":[
-            { "title": "...", "content": "create in markdown format, not just plain text but an entire lesson content", "difficulty": 1|2|3, "order": 1|2|3... },]
+            { "title": "...", "content": "create in markdown format, not just short text or description but an entire lesson content, content format should be similar compared to this resource https://www.ef.com/wwen/english-resources/english-grammar/noun-gender/", "difficulty": 1|2|3, "order": 1|2|3... },]
        }
 2. For each course, generate 5 multiple-choice questions.
    Each question must include:
@@ -80,6 +81,17 @@ Instructions:
      "courses": [ {course1}, {course2}, {course3} ]
    }
 4. Ensure the JSON is valid and parsable.
+5. Fill all the checklist before generating the output:
+        [ ] Total count of courses generated = 3?
+        [ ] Courses has different difficulties?
+        [ ] Total questions generated = 5 for each courses?
+        [ ] Courses generated has no null value?
+        [ ] Questions generated has no null value?
+        [ ] Courses generated has valid JSON format with strict equality?
+        [ ] Questions generated has valid JSON format with strict equality?
+        [ ] Course content is in markdown format?
+        [ ] Course content format is similar to the example resource, not just a single one liner description?
+6. If there is ONE checklist from instruction no. 5 that is not filled, re-iterate until everything is filled (IMPORTANT!)
         `,
       });
       const clean = response.text.replace(/```json|```/g, "").trim();
